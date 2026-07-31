0b823f0e1e17d9e9487e711e4ec7847f807a4376
theoleonardblanc-max <theoleonardblanc@gmail.com>
2026-07-31 13:21:12 +0200

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -468,8 +468,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
